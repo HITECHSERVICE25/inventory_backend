e73fab26966cac1898b694803b52e97a878b76fa
HITECHSERVICE25 <maniklalbera1945@gmail.com>
2026-03-08 09:36:02 +0530

    removed installation module

--- src/middlewares/validations/installationValidation.js
+++
@@ -1,7 +0,0 @@
-const { body } = require('express-validator');
-
-exports.validateChargeUpdate = [
-  body('amount')
-    .isFloat({ min: 0 })
-    .withMessage('Valid positive number required')
-];
